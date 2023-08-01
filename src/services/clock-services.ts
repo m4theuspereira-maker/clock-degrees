02897327308d7b3afc95f91300e2797476045347
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-08-01 18:20:53 -0300

    refactor: add round for return

--- src/services/clock-services.ts
+++ src/services/clock-services.ts
@@ -2,7 +2,7 @@ export class ClockServices {
   calculateDegress(hour: number, minute: number = 0) {
 
     // Calcular a distância angular
-    const hourAngle = (hour % 12) * 30 + minute * 0.5; // Cada hora tem 30 graus (360/12) e cada minuto adicional acrescenta 0.5 graus.
+    const hourAngle = (hour % 12) * 30 + minute * 0.6; // Cada hora tem 30 graus (360/12) e cada minuto adicional acrescenta 0.5 graus.
     const minuteAngle = minute * 6; // Cada minuto tem 6 graus (360/60).
 
     // Calcular a diferença absoluta entre os ângulos
@@ -13,6 +13,6 @@ export class ClockServices {
       angularDistance = 360 - angularDistance;
     }
 
-    return { angle: angularDistance };
+    return { angle: Math.round(angularDistance) };
   }
 }
