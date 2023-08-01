de75f74305d0aca43d88a7b2811eeddfa50ee016
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-08-01 17:48:22 -0300

    refactor: add trycatch

--- src/controllers/clock-controller.ts
+++ src/controllers/clock-controller.ts
@@ -1,17 +1,21 @@
 import { Request, Response } from "express";
 import { ClockServices } from "../services/clock-services";
-import { ok } from "./handlers/handles";
+import { ok, serverError } from "./handlers/handles";
 export class ClockController {
   constructor(private readonly clockServices: ClockServices) {}
 
   getDegress = async (req: Request, res: Response) => {
-    const { hour, minute } = req.params as unknown as {
-      hour: number;
-      minute: number;
-    };
+    try {
+      const { hour, minute } = req.params as unknown as {
+        hour: number;
+        minute: number;
+      };
 
-    const result = await this.clockServices.calculateDegress(hour, minute);
+      const result = await this.clockServices.calculateDegress(hour, minute);
 
-    return ok(res, result);
+      return ok(res, result);
+    } catch (error) {
+      return serverError(res, error);
+    }
   };
 }
