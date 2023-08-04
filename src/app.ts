412403210db4edce207b13a728ab44644f993070
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2023-08-04 11:48:44 -0300

    feat: add documentation json

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import { routes } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import { SWAGGER_DOCS } from "./common/environment-consts";
 
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cors());
+app.use("/api", swaggerUi.serve, swaggerUi.setup(SWAGGER_DOCS));
 app.use(routes);
 
 export { app };
--- src/common/environment-consts.ts
+++ src/common/environment-consts.ts
@@ -1,6 +1,8 @@
 import { resolve } from "path";
 import { config } from "dotenv";
+import swaggerDocs from "./swagger.json";
 
 config({ path: resolve(__dirname, "../../.env") });
 
 export const PORT = process.env.PORT || 3000;
+export const SWAGGER_DOCS = swaggerDocs;
